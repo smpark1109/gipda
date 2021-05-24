183596960b6ecea9a7d10f41e1880f9156e26acd
smpark1109 <81151065+smpark1109@users.noreply.github.com>
2021-05-24 20:31:03 +0900

    commit

--- capston/settings.py
+++ capston/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-l*a-t$vf69^t9%j_3j58%h=c7+zb_cq1=_b5s59*%(3c5ue62b')
+SECRET_KEY = 'DJANGO_SECRET_KEY', 'django-insecure-l*a-t$vf69^t9%j_3j58%h=c7+zb_cq1=_b5s59*%(3c5ue62b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
